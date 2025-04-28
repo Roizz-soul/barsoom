c9e01bb55bc7e4ff386f959556b2cb40294abddc
Roizz-soul <106694609+Roizz-soul@users.noreply.github.com>
2025-04-28 19:06:12 +0000

    Try take

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import * as core from '@actions/core';
 import * as github from '@actions/github'
 
 try {
-    const nameToGreet = core.getInput('name');
-    const output_value = "Hello " + nameToGreet;
-    core.setOutput('outputKey', output_value);
+    const name = core.getInput('name');
+    const output_value = "Hello " + name;
+    core.setOutput('greeting', output_value);
 } catch(error) {
     core.setFailed(error.message);
 }
